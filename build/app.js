6e5e7ab046e8c988fdb99155b856cd0fb94af788
moschan <kaityo02@gmail.com>
2015-03-18 21:24:16 +0900

    jqueryをreactに置き換え

--- build/app.js
+++ build/app.js
@@ -1,13 +1,21 @@
 $(function () {
     // chrome tab example
     chrome.tabs.getSelected(null, function(tab) {
-        $('#title').text(tab.title);
-        $('#url').text(tab.url);
+        /* React + JSX */
+        React.render(
+          React.createElement("h1", null, "Hello, React!"),
+          document.getElementById('heading')
+        );
+
+        React.render(
+          React.createElement("p", null,  tab.title),
+          document.getElementById('title')
+        );
+
+        React.render(
+          React.createElement("p", null,  tab.url),
+          document.getElementById('url')
+        );
     });
 
-    /* React + JSX */
-    React.render(
-      React.createElement("h1", null, "Hello, React!"),
-      document.getElementById('heading')
-    );
 });
--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
+// ビルド
+// jsx --watch js build
+//
 $(function () {
     // chrome tab example
     chrome.tabs.getSelected(null, function(tab) {
-        $('#title').text(tab.title);
-        $('#url').text(tab.url);
+        /* React + JSX */
+        React.render(
+          <h1>Hello, React!</h1>,
+          document.getElementById('heading')
+        );
+
+        React.render(
+          <p>{ tab.title }</p>,
+          document.getElementById('title')
+        );
+
+        React.render(
+          <p>{ tab.url }</p>,
+          document.getElementById('url')
+        );
     });
 
-    /* React + JSX */
-    React.render(
-      <h1>Hello, React!</h1>,
-      document.getElementById('heading')
-    );
 });
