d77e4a9925cd6bdf5a25f738710337d5636ca0dc
moschan <kaityo02@gmail.com>
2015-03-20 13:15:41 +0900

    分割

--- build/app.js
+++ build/app.js
@@ -6,13 +6,29 @@ $(function () {
     // chrome tab example
     chrome.tabs.getSelected(null, function(tab) {
 
+        var Component_title = React.createClass({displayName: "Component_title",
+          render : function() {
+            return (
+                React.createElement("p", {id: "title"},  tab.title)
+            )
+          }
+        });
+
+        var Component_url = React.createClass({displayName: "Component_url",
+          render : function() {
+            return (
+                React.createElement("p", {id: "url"},  tab.url)
+            )
+          }
+        });
+
         var Component = React.createClass({displayName: "Component",
           render : function() {
             return (
               React.createElement("div", {class: "Component-inner"}, 
                 React.createElement("h1", {className: "commentForm"}, "Hello, React!"), 
-                React.createElement("p", {id: "title", className: "commentForm"},  tab.title), 
-                React.createElement("p", {id: "url", className: "commentForm"},  tab.url)
+                React.createElement(Component_title, null), 
+                React.createElement(Component_url, null)
               )
             )
           }
--- js/app.js
+++ js/app.js
@@ -6,13 +6,29 @@ $(function () {
     // chrome tab example
     chrome.tabs.getSelected(null, function(tab) {
 
+        var Component_title = React.createClass({
+          render : function() {
+            return (
+                <p id="title">{ tab.title }</p>
+            )
+          }
+        });
+
+        var Component_url = React.createClass({
+          render : function() {
+            return (
+                <p id="url">{ tab.url }</p>
+            )
+          }
+        });
+
         var Component = React.createClass({
           render : function() {
             return (
               <div class="Component-inner">
                 <h1 className="commentForm">Hello, React!</h1>
-                <p id="title" className="commentForm">{ tab.title }</p>
-                <p id="url" className="commentForm">{ tab.url }</p>
+                <Component_title />
+                <Component_url />
               </div>
             )
           }
